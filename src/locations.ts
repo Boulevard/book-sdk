2a94d5d8cd160a17e184d4b4e26425da2d646bd3
James Bradley <jbradley@blvd.co>
2021-06-10 19:02:41 +0100

    Document Locations.list

--- src/locations.ts
+++ src/locations.ts
@@ -8,6 +8,13 @@ class Locations {
    */
   constructor(private platformClient: PlatformClient) {}
 
+  /**
+   * List locations for the business
+   *
+   * @async
+   * @returns Promise containing a list of Locations
+   * @todo Pagination
+   */
   async list(): Promise<Array<Location>> {
     const response = await this.platformClient.request(getLocationsQuery);
     return response.locations.edges.map((edge: LocationEdge) => edge.node);
