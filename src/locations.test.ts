895ffb45df774476c3096078dacb5f5d778266b7
James Bradley <jbradley@blvd.co>
2021-06-14 16:09:11 +0100

    Separate platform-specific tests

--- src/locations.test.ts
+++ src/locations.test.ts
@@ -1,7 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { Blvd } from "./blvd";
 import fetch from "cross-fetch";
-import { PlatformTarget } from "./platformClient";
 
 jest.mock("cross-fetch", () => ({
   __esModule: true,
@@ -27,42 +26,7 @@ describe("testing api", () => {
     client.locations.list();
 
     expect(fetch).toHaveBeenCalledWith(
-      `https://sandbox.joinblvd.com/api/2020-01/${businessID}/client`,
-      expect.objectContaining({
-        body: expect.stringContaining("locations(first: 100)")
-      })
-    );
-  });
-
-  test("fetches live locations", () => {
-    const businessID = uuidv4();
-    const apiKey = uuidv4();
-    const client = new Blvd(apiKey, businessID, PlatformTarget.Live);
-
-    client.locations.list();
-
-    expect(fetch).toHaveBeenCalledWith(
-      `https://dashboard.boulevard.io/api/2020-01/${businessID}/client`,
-      expect.objectContaining({
-        body: expect.stringContaining("locations(first: 100)")
-      })
-    );
-  });
-
-  test("fetches local locations", () => {
-    const businessID = uuidv4();
-    const apiKey = uuidv4();
-    const client = new Blvd(
-      apiKey,
-      businessID,
-      // @ts-expect-error Testing undocumented feature for internal development
-      "http://localhost:4000"
-    );
-
-    client.locations.list();
-
-    expect(fetch).toHaveBeenCalledWith(
-      `http://localhost:4000/api/2020-01/${businessID}/client`,
+      expect.any(String),
       expect.objectContaining({
         body: expect.stringContaining("locations(first: 100)")
       })
---
+++ src/platformClient.test.ts
@@ -0,0 +1,85 @@
+import fetch from "cross-fetch";
+import { PlatformClient, PlatformTarget } from "./platformClient";
+
+jest.mock("cross-fetch", () => ({
+  __esModule: true,
+  ...jest.requireActual("cross-fetch"),
+  default: jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      headers: { get: () => "application/json" },
+      json: () => Promise.resolve({ data: {} })
+    })
+  )
+}));
+
+jest.mock("cross-fetch");
+
+describe("testing api", () => {
+  test("fetches sandbox locations", () => {
+    const businessID = "0bfcf11-4eed-4118-bbbc-8d7f7a1c5d8a";
+    const apiKey = "197f3eac-f90d-42b3-80ed-e3a68288647b";
+    const client = new PlatformClient(apiKey, businessID);
+    client.request("");
+
+    expect(fetch).toHaveBeenCalledWith(
+      "https://sandbox.joinblvd.com/api/2020-01/0bfcf11-4eed-4118-bbbc-8d7f7a1c5d8a/client",
+      {
+        body: '{"query":""}',
+        headers: {
+          Authorization:
+            "Basic MTk3ZjNlYWMtZjkwZC00MmIzLTgwZWQtZTNhNjgyODg2NDdiOg==",
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }
+    );
+  });
+
+  test("fetches live locations", () => {
+    const businessID = "81e9fda3-98d2-4b91-86af-406e1db8d30b";
+    const apiKey = "a7227ad0-9042-4e8d-9349-332ab6544027";
+    const client = new PlatformClient(apiKey, businessID, PlatformTarget.Live);
+
+    client.request("");
+
+    expect(fetch).toHaveBeenCalledWith(
+      "https://dashboard.boulevard.io/api/2020-01/81e9fda3-98d2-4b91-86af-406e1db8d30b/client",
+      {
+        body: '{"query":""}',
+        headers: {
+          Authorization:
+            "Basic YTcyMjdhZDAtOTA0Mi00ZThkLTkzNDktMzMyYWI2NTQ0MDI3Og==",
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }
+    );
+  });
+
+  test("fetches local locations", () => {
+    const businessID = "aa548528-7bc7-47c3-a063-37c950a6e5c2";
+    const apiKey = "265e5365-b742-409f-98ef-d890b3efde70";
+    const client = new PlatformClient(
+      apiKey,
+      businessID,
+      // @ts-expect-error Testing undocumented feature for internal development
+      "http://localhost:4000"
+    );
+
+    client.request("");
+
+    expect(fetch).toHaveBeenCalledWith(
+      "http://localhost:4000/api/2020-01/aa548528-7bc7-47c3-a063-37c950a6e5c2/client",
+      {
+        body: '{"query":""}',
+        headers: {
+          Authorization:
+            "Basic MjY1ZTUzNjUtYjc0Mi00MDlmLTk4ZWYtZDg5MGIzZWZkZTcwOg==",
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }
+    );
+  });
+});
